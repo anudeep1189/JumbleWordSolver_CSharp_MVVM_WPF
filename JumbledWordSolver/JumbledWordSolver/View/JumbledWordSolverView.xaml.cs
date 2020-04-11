0d8774a352f8d1a03f0ed5f37c2bdc40e69be41f
anudeep <13384863+anudeep1189@users.noreply.github.com>
2020-04-11 13:25:59 +0530

    UI changes adding windows dialog for selecting the file name

--- JumbledWordSolver/JumbledWordSolver/View/JumbledWordSolverView.xaml.cs
+++ JumbledWordSolver/JumbledWordSolver/View/JumbledWordSolverView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,16 @@ namespace JumbledWordSolver.View
         public JumbledWordsView()
         {
             InitializeComponent();
+        }
+
+        private void btnClick_OpenBrowser(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; ;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath.Text = openFileDialog.FileName;
+            }
         }
     }
 }
